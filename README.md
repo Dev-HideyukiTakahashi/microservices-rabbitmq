c8047828fa9d74817ac8c102a4605994b0d30a1a
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-12-16 00:39:38 -0300

    criar projeto pedidos-api

---
+++ README.md
@@ -0,0 +1,4 @@
+# Spring boot with Rabbitmq example
+
+- Img rabbitmq cmd
+  - `docker run -d --name my_rabbitmq -p 15672:15672 -e RABBITMQ_DEFAULT_USER=rabbitmq -e RABBITMQ_DEFAULT_PASS=rabbitmq rabbitmq:3.10.5-management`
---
+++ pedidos-api/src/test/java/br/com/example/pedidos/api/PedidosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.pedidos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
