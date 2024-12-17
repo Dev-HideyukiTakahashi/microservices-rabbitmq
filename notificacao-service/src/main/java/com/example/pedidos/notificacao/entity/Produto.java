ea68b47841565d84a9dfdb2c265cdbca0a9dd7af
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-12-17 18:55:26 -0300

    notificao model

---
+++ notificacao-service/src/main/java/com/example/pedidos/notificacao/entity/Produto.java
@@ -0,0 +1,16 @@
+package com.example.pedidos.notificacao.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produto {
+
+    private UUID id = UUID.randomUUID();
+    private String nome;
+    private Double valor;
+}
