778c6c7482c1f35c8e10f0f22d65386ba51b033b
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-12-17 20:21:23 -0300

    testando mailhog
    Simulando envio de email com mailhog
    conferindo na porta 8025 conforme
    docker compose config mailhog

--- notificacao-service/src/main/java/com/example/pedidos/notificacao/listener/PedidoListener.java
+++ notificacao-service/src/main/java/com/example/pedidos/notificacao/listener/PedidoListener.java
@@ -1,5 +1,6 @@
 package com.example.pedidos.notificacao.listener;
 
+import com.example.pedidos.notificacao.service.EmailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -12,8 +13,15 @@ public class PedidoListener {
 
     private final Logger logger = LoggerFactory.getLogger(PedidoListener.class);
 
+    private final EmailService emailService;
+
+    public PedidoListener(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues = "pedidos.v1.pedido-criado.gerar-notificao")
     public void enviarNotificacao(Pedido pedido) {
+        emailService.enviarEmail(pedido);
         logger.info("Notificação gerada: {}", pedido.toString());
     }
 }
