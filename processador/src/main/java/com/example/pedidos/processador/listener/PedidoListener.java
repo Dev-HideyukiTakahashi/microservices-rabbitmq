445be3b3c5c1b90d637b3c51b33847c556c9d69f
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-12-18 00:01:55 -0300

    salvando no bd

--- processador/src/main/java/com/example/pedidos/processador/listener/PedidoListener.java
+++ processador/src/main/java/com/example/pedidos/processador/listener/PedidoListener.java
@@ -2,6 +2,7 @@ package com.example.pedidos.processador.listener;
 
 import com.example.pedidos.processador.entity.Pedido;
 import com.example.pedidos.processador.entity.enums.Status;
+import com.example.pedidos.processador.service.PedidoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -11,10 +12,15 @@ import org.springframework.stereotype.Component;
 public class PedidoListener {
 
     private final Logger logger = LoggerFactory.getLogger(PedidoListener.class);
+    private final PedidoService pedidoService;
+
+    public PedidoListener(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
 
     @RabbitListener(queues = "pedidos.v1.pedido-criado.gerar-processamento")
     public void salvarPedido(Pedido pedido){
         pedido.setStatus(Status.PROCESSADO);
-        logger.info("Pedido processado: {}" , pedido.toString());
+        pedidoService.salvar(pedido);
     }
 }
